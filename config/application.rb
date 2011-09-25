cbfbe680afb932b917612f0a236b9f0ddbd28545
Josh Rendek <josh@bluescripts.net>
2011-09-25 09:15:56 -0400

    bundler stuff

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module Classly
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.assets.precompile << '*.css'
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
