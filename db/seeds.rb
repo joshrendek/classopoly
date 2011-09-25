70e197855ad62e52766c6b1483e608abb2cd2598
Josh Rendek <josh@bluescripts.net>
2011-09-24 23:08:20 -0400

    bundler stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.connection.execute(File.open("#{Rails.root}/db/seeds/courses_colleges.sql").to_s)
